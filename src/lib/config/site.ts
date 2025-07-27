7ff95540a84bb5905d66b15e6a3b5f52624e00f2
Abhiram <abhi6945@gmail.com>
2025-07-27 19:17:49 -0400

    Create site.ts

---
+++ src/lib/config/site.ts
@@ -0,0 +1,8 @@
+export const SITE = {
+  title: 'Abhiram Singireddy',
+  description: 'Student. Exploring systems with curiosity and precision.',
+  email: 'notifysec@proton.me',
+  github: 'https://github.com/as4395',
+  linkedin: 'https://linkedin.com/in/abhiram-singireddy',
+  url: 'https://example.org,
+};
