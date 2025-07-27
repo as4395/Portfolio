8d1fdf0f136c47648543e1bddae3826322882f78
Abhiram <abhi6945@gmail.com>
2025-07-27 19:16:32 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import ThemeToggle from '$lib/components/ThemeToggle.svelte';
   import { onMount } from 'svelte';
 
   let colorScheme = 'light dark';
@@ -15,11 +16,9 @@
     if (scheme === 'light' || scheme === 'dark') {
       html.classList.add(scheme);
     } else {
-      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        html.classList.add('dark');
-      } else {
-        html.classList.add('light');
-      }
+      html.classList.add(
+        window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+      );
     }
     localStorage.setItem('color-scheme', scheme);
   }
@@ -33,4 +32,10 @@
 
 <div class="min-h-screen px-4 sm:px-6 md:px-8 max-w-3xl mx-auto py-20 space-y-24">
   <slot />
+
+  <!-- Footer -->
+  <footer class="pt-10 mt-20 border-t border-neutral-200 dark:border-neutral-700 text-sm flex justify-between items-center">
+    <div class="italic text-neutral-500 dark:text-neutral-400">Never stop creating.</div>
+    <ThemeToggle />
+  </footer>
 </div>
