107b1a65e7286aa99a413a88e384d7ea429ebfcd
Abhiram <abhi6945@gmail.com>
2025-07-29 22:24:26 -0400

    Create NavMenu.svelte

---
+++ src/lib/components/NavMenu.svelte
@@ -0,0 +1,17 @@
+<script>
+  const links = [
+    { name: 'Sokolov', href: '/' },
+    { name: '2025', href: '/2025' },
+    { name: 'About', href: '/about' },
+    { name: 'Guides', href: '/guides' },
+    { name: 'Contact', href: '/contact' }
+  ];
+</script>
+
+<nav class="flex gap-4 text-sm mb-12 justify-center text-neutral-600 dark:text-neutral-400">
+  {#each links as link}
+    <a href={link.href} class="hover:text-black dark:hover:text-white transition">
+      {link.name}
+    </a>
+  {/each}
+</nav>
