6acf4d472fa582d880848c37984ad488df4bb4a6
Abhiram <abhi6945@gmail.com>
2025-07-30 22:40:11 -0400

    Create BackLink.svelte

---
+++ src/lib/components/BackLink.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let href = '/';
+  export let label = '← back';
+</script>
+
+<a href={href} class="text-sm text-neutral-500 hover:text-neutral-900 dark:hover:text-white transition">
+  {label}
+</a>
+
