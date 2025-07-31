5031c3571c59bc0505b2e8c7060d662e71d0380d
Artyom Sokolov <abhi6945@gmail.com>
2025-07-31 17:26:40 -0400

    Update +page.svelte

--- src/routes/guides/[slug]/+page.svelte
+++ src/routes/guides/[slug]/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+  import LayoutContainer from '$lib/components/LayoutContainer.svelte';
+  import BackLink from '$lib/components/BackLink.svelte';
   export let data;
 </script>
 
-<article class="prose dark:prose-invert max-w-none">
-  {@html data.content}
-</article>
+<LayoutContainer>
+  <BackLink href="/guides" />
+  <article class="prose dark:prose-invert" bind:this>
+    {@html data.post.content}
+  </article>
+</LayoutContainer>
