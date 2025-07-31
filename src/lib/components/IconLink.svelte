fc46d69b79e9f218346f7a282cf2b09e9ab27dc0
Artyom Sokolov <abhi6945@gmail.com>
2025-07-31 17:34:55 -0400

    Create IconLink.svelte

---
+++ src/lib/components/IconLink.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let href: string;
+  export let label: string;
+</script>
+
+<a href={href} target="_blank" rel="noopener noreferrer" class="hover:underline text-sm text-neutral-700 dark:text-neutral-300">
+  {label} ↗
+</a>
