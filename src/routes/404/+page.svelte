2d673937f553e695def0c87606735d7b345a1049
Abhiram <abhi6945@gmail.com>
2025-07-30 22:25:16 -0400

    Create +page.svelte

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,6 @@
+<section class="space-y-6 text-center py-24 animate-fade-in-up">
+  <h1 class="text-3xl font-medium">Page not found.</h1>
+  <p class="text-neutral-600 dark:text-neutral-400">
+    This part of the archive hasn’t been written yet.
+  </p>
+</section>
