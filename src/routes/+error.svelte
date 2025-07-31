3398dc7fe8b436eed423dcbe42bb18413b324e33
Abhiram <abhi6945@gmail.com>
2025-07-30 22:24:57 -0400

    Create +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let error;
+</script>
+
+<section class="space-y-6 text-center py-24 animate-fade-in-up">
+  <h1 class="text-3xl font-medium">Hmm.</h1>
+  <p class="text-neutral-600 dark:text-neutral-400">
+    Something went wrong. {error.message}
+  </p>
+</section>
