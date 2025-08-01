35077b35db86b007a7282cf0f8be34653520b484
Artyom Sokolov <abhi6945@gmail.com>
2025-07-31 21:01:09 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 export default {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
-    base: '/portfolio-site/'
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/portfolio-site', 
+    }
   }
 };
