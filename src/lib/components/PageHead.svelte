97cb78a99bd9a69699170e53118d51c5c02af07c
Artyom Sokolov <abhi6945@gmail.com>
2025-07-31 20:42:39 -0400

    Create PageHead.svelte

---
+++ src/lib/components/PageHead.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { pageTitle, pageDescription } from '$lib/stores/pageMeta';
+  export let title = 'Sokolov';
+  export let description = 'Portfolio + Personal Website.';
+  $: pageTitle.set(title);
+  $: pageDescription.set(description);
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description} />
+</svelte:head>
